0d1da06159ce3801c001fb0aa4cfbedcc21094ef
fridi <fridi.beims@gmail.com>
2025-12-03 16:17:13 +0100

    feat(net): add seed parameter for weight initialization in NNNet class

--- net.py
+++ net.py
@@ -1,28 +1,31 @@
 import random
 import math
 
-def rand():
-    return random.uniform(-0.1, 0.1)
+
 
 class NNNet():
 
     # defining random weight for each loayer and ech neuron and initilaizing the neuronal network
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, seed=None):
+        random.seed(seed)
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
 
         # random weighted inputs for hidden layer
-        self.W1 = [[rand() for _ in range(self.input_size)]
+        self.W1 = [[self.rand() for _ in range(self.input_size)]
                    for _ in range(self.hidden_size)]
         self.b1 = [0.0 for _ in range(self.hidden_size)]
 
         # random weighted inputs for output layer
-        self.W2 = [[rand() for _ in range(self.hidden_size)]
+        self.W2 = [[self.rand() for _ in range(self.hidden_size)]
                    for _ in range(self.output_size)]
         self.b2 = [0.0 for _ in range(self.output_size)]
 
         self.learning_rate = 0.1
+
+    def rand(self):
+        return random.uniform(-0.1, 0.1)
 
     # translates big values into values between 0 and 1
     def sigmoid(self, x):
